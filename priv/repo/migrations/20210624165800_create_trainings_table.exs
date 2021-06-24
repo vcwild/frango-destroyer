4b0e0bcf5cc5834d70caeeffe21c56bb437ef799
Victor <vcwildm@gmail.com>
2021-06-24 18:40:44 -0300

    create training migration

---
+++ priv/repo/migrations/20210624165800_create_trainings_table.exs
@@ -0,0 +1,13 @@
+defmodule FrangoDestroyer.Repo.Migrations.CreateTrainingsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:trainings) do
+      add :start_date, :date
+      add :end_date, :date
+      add :user_id, references(:users)
+
+      timestamps()
+    end
+  end
+end
